4ed6a81912096a39ae0919946f6d966468f9c753
Tom-Mathis Chapuis <tom-mathis.chapuis@epitech.eu>
2026-01-02 08:57:19 +0100

    feat(frontend): add responsive optimizations and ResponsiveDialog

--- frontend/tailwind.config.ts
+++ frontend/tailwind.config.ts
@@ -18,6 +18,14 @@ const config = {
   		}
   	},
   	extend: {
+  		screens: {
+  			xs: '375px',
+  			sm: '640px',
+  			md: '768px',
+  			lg: '1024px',
+  			xl: '1280px',
+  			'2xl': '1536px'
+  		},
   		colors: {
   			border: 'hsl(var(--border))',
   			input: 'hsl(var(--input))',
